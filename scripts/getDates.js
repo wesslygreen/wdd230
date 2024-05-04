b6e73e1c41153da8c5e1907af91683f0573d5865
wesslygreen <gre21008@byui.edu>
2024-05-04 16:37:09 -0600

    index, base, larger

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+// get current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// get last modified date
+document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
